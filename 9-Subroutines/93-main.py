5e1181ab83a784d4ed092e9b383a43d8008f980f
sawubona <mail@sawubona.ch>
2024-07-16 16:49:18 +0200

    Create 93-main.py

---
+++ 9-Subroutines/93-main.py
@@ -0,0 +1,15 @@
+
+
+def location(country): # Rename the function so that it has a sensible name. 
+  print("I am from " + country)
+
+
+location("Brazil") #Don't forget to rename it when it is called.
+
+location("Sweden") # Call the function with the argument 'Sweden'
+
+
+# Get the user to input a country. Store it in a variable. Call the function with the variable as the argument.
+print("Where are you from?")
+country = input
+location(country)
